6672e0dda493fbbe34dbf52456cc125e5b106f33
Masum Rahman <masumrahman73@gmail.com>
2023-07-14 16:54:44 +0600

    instructor add class and my class added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 // mongodb
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wbbftwk.mongodb.net/?retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,6 +28,7 @@ async function run() {
 
     // collection build
     const usersCollection = client.db('musicDB').collection('users');
+    const classCollection = client.db('musicDB').collection('class');
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
@@ -45,8 +46,8 @@ async function run() {
     app.get('/users/:email', async (req, res) => {
       const email = req.params.email;
 
-      const result = await usersCollection.findOne({email: email});
-      res.send({data: result});
+      const result = await usersCollection.findOne({ email: email });
+      res.send({ data: result });
     });
 
     app.post('/users', async (req, res) => {
@@ -58,9 +59,62 @@ async function run() {
         return res.send({ message: 'user already exists' })
       }
 
-      const result = await usersCollection.insertOne({...user, role:"student"});
+      const result = await usersCollection.insertOne({ ...user, role: "student" });
       res.send(result);
     });
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+    app.patch('/users/instructor/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'instructor'
+        },
+      };
+
+
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
+    // class - instructor related apis 
+    app.post('/addclass', async (req, res) => {
+      const newItem = req.body;
+      const result = await classCollection.insertOne({ ...newItem, status: "pending", instructor: new ObjectId(newItem.instructor), enrolledStudents: 0, feedback: '' })
+      res.send(result);
+    })
+    app.get('/classes', async (req, res) => {
+      const result = await classCollection.find().toArray();
+      res.send(result);
+    });
+    app.patch('/updateclass/:id', async (req, res) => {
+      const id = req.params.id;
+      const updateClass = req.body;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: updateClass,
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
